6af3bb45068dfa8e50ec91d54dc64e460c282e31
Mina <minatari@uw.edu>
2015-11-21 18:17:18 -0800

    trouble w/ basic rendering state

--- js/app.js
+++ js/app.js
@@ -1,46 +1,37 @@
+//js for cat finding food game
+//my cat: bucky
+//his reign: terror
+
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
     var canvas = document.getElementById('game-canvas');
-
-    //2D rendering context
     var ctx = canvas.getContext('2d');
-
-    //current game state
     var gameState;
 
-    //create a new game state object and return it
     function newGameState() {
-        //creates new object to return
         return {
-            //create yet another object for object property
-            ball: {
-                //coordinates, size
-                //left: 10,
-                //random is between 0 or 1, good for a percentage
-                left: 240,
-                top: 20,
+            bucky: {
+                //start position
+                left: 10,
+                top: 10,
+
+                //size
                 width: 10,
                 height: 10,
 
                 //direction, velocity
-                //always moves right one pixel, down one pixel
-                vectorX: 3,
-                vectorY: 0,
-                velocity: 0
+                vectorX: 1,
+                vectorY: 1
             },
-            //gives microsecond accurate value for timestamp
             lastTimestamp: performance.now()
         };
-    } //newGameState()
+    } //newGameState
 
-    //render current game state to canvas element
     function render() {
-        //clears entire canvas
-        //get rekt (rect)
+        //clears entire canvas (get re(k)ct)
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        var ball = gameState.ball;
+        var bucky = gameState.bucky;
 
         //begins the path
         ctx.beginPath();
@@ -53,65 +44,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
         //fills in the circle
         ctx.fill();
-    } //render()
 
-    //advance the animation and redraw
-    function animate() {
+    }
+
+    function step() {
         render();
-        requestAnimationFrame(animate);
-    }
-
-    window.addEventListener("keydown", moveChar, false);
-
-    function moveChar(evt) {
-        var ball = gameState.ball;
-        var movement = 30;
-        switch(evt.keyCode) {
-            //WSAD (up,down,left,right) --> (87, 83, 65, 68)
-            case 37:
-                // left key pressed
-                ball.left -= movement;
-                if (ball.left + ball.width <= ball.width) {
-                    ball.left = 0;
-                }
-                break;
-
-            case 38:
-                // up key pressed
-                ball.height -= movement;
-                //if (ball.top <= 0) {
-                //    ball.top = ball.height;
-                //}
-                break;
-
-            case 39:
-                // right key pressed
-                ball.left += movement;
-                if (ball.left + ball.width >= canvas.width) {
-                    ball.left = canvas.width - ball.width;
-                }
-                break;
-            case 40:
-                // down key pressed
-                ball.height += movement;
-                if (ball.top >= canvas.height) {
-                    alert("BLERG");
-                }
-                break;
-
-            case 32:
-                //space bar pressed
-                ball.height += movement * 5;
-                break;
-        }
     }
 
 
-    //create a new game state
-    gameState = newGameState();
+    //WSAD (up,down,left,right) --> (87, 83, 65, 68)
 
-    //ask browser to animate ask quickly as possible
-    //global function
+gameState = newGameState();
     requestAnimationFrame(animate);
-
 });
---
+++ js/oldmove.js
@@ -0,0 +1,135 @@
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    var canvas = document.getElementById('game-canvas');
+
+    //2D rendering context
+    var ctx = canvas.getContext('2d');
+
+    //current game state
+    var gameState;
+
+    //create a new game state object and return it
+    function newGameState() {
+        //creates new object to return
+        return {
+            //create yet another object for object property
+            ball: {
+                //start coordinates
+                left: 50,
+                top: 50,
+
+                //size
+                width: 10,
+                height: 10,
+
+                //direction, velocity
+                vectorX: 1,
+                vectorY: 1,
+                velocity: 3
+            },
+            lastTimestamp: performance.now()
+        };
+    } //newGameState()
+
+    //render current game state to canvas element
+    function render() {
+        //clears entire canvas
+        //get rekt (rect)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        //reference
+        var ball = gameState.ball;
+
+        //begins the path
+        ctx.beginPath();
+
+        //draws the arc
+        //takes in center coordinates of circle
+        ctx.arc(ball.left + (ball.width / 2),
+            ball.top + (ball.height / 2),
+            ball.width / 2, 0, 2 * Math.PI);
+
+        //fills in the circle
+        ctx.fillStyle = "#DA4453";
+        ctx.fill();
+
+        //shows me coordinates
+        var idx;
+        for(idx = 0; idx < canvas.width + 20; idx += 20) {
+            //print, x-coord, y-coord
+            ctx.fillText(idx, idx, 10);
+        }
+
+        for(idx = 0; idx < canvas.height + 20; idx += 20) {
+            ctx.fillText(idx, 0, idx);
+        }
+    } //render()
+
+    //advance the animation and redraw
+    function animate(timestamp) {
+        render();
+        requestAnimationFrame(animate);
+    }
+
+    //add movement by arrow keys
+    document.addEventListener("keydown", function moveChar(evt) {
+        var ball = gameState.ball;
+        var movement = 10;
+        switch(evt.keyCode) {
+            //WSAD (up,down,left,right) --> (87, 83, 65, 68)
+            case 37:
+                // left key pressed
+                ball.left -= movement;
+
+                //dont hit walls
+                if (ball.left + ball.width <= ball.width) {
+                    ball.left = 0;
+                }
+                break;
+
+            case 38:
+                // up key pressed
+                ball.top -= movement;
+
+                //dont hit walls
+                if (ball.top <= 0) {
+                    ball.top = 0;
+                }
+                break;
+
+            case 39:
+                // right key pressed
+                ball.left += movement;
+
+                //dont hit walls
+                if (ball.left + ball.width >= canvas.width) {
+                    ball.left = canvas.width - ball.width;
+                }
+                break;
+            case 40:
+                // down key pressed
+                ball.top += movement;
+
+                //dont hit walls
+                if (ball.top + ball.height >= canvas.height) {
+                    ball.top = canvas.height - ball.height;
+                }
+
+                break;
+
+            case 32:
+                //space bar pressed
+                //do an animation
+                break;
+        }
+    });
+
+    //create a new game state
+    gameState = newGameState();
+
+    //ask browser to animate ask quickly as possible
+    //global function
+    requestAnimationFrame(animate);
+
+});

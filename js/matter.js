a4d4ccdb6c5c818d84792911ad5572ee119e2c20
Mina <minatari@uw.edu>
2015-11-24 14:15:32 -0800

    markdown testing

--- js/matter.js
+++ js/matter.js
@@ -8,7 +8,10 @@ document.addEventListener('DOMContentLoaded', function() {
         var Engine = Matter.Engine,
             World = Matter.World,
             Bodies = Matter.Bodies,
-            MouseConstraint = Matter.MouseConstraint;
+            MouseConstraint = Matter.MouseConstraint,
+            Common = Matter.Common,
+            Composites = Matter.Composites;
+
 
         // create a Matter.js engine
         var engine = Engine.create(document.body, {
@@ -21,8 +24,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
         //adjust gravity
-        engine.world.gravity.x = 0.5;
-        engine.world.gravity.y = 0.7;
+        engine.world.gravity.x = 0;
+        engine.world.gravity.y = 0;
 
         //create offset for walls
         var offset = 10,
@@ -32,8 +35,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     visible: false
                 }
             };
-
-        engine.world.bodies = [];
 
         //create static walls
         World.add(engine.world, [
@@ -47,13 +48,26 @@ document.addEventListener('DOMContentLoaded', function() {
         var mouseConstraint = MouseConstraint.create(engine);
         World.add(engine.world, mouseConstraint);
 
+        //add light distraction objects
+        var stacklight = Composites.stack(30, 30, 20, 5, 60, 40, function(x, y, column, row) {
+            return Bodies.circle(x, y, Common.random(10, 25), { friction: 0.0001, restitution: 0.5, density: 0.5 });
+        });
+
+        //add heavy distraction objects
+        var stackheavy = Composites.stack(10, 10, 10, 15, 20, 10, function(x, y, column, row) {
+            return Bodies.circle(x, y, Common.random(20, 25), { friction: 0.0001, restitution: 0.5, density: 0.0001 });
+        });
+
+        // add distraction objects to the world
+        World.add(engine.world, stacklight, stackheavy);
 
         // create bucky objects
         //pos(x, y), width, height
-        var buckyA = Bodies.rectangle(400, 200, 80, 80);
-        var buckyB = Bodies.rectangle(450, 50, 80, 180);
-        var buckyC = Bodies.rectangle(450, 150, 180, 80);
-        var buckyD = Bodies.rectangle(70, 70, 200, 200);
+        var buckyA = Bodies.circle(400, 200, 20, 20);
+        var buckyB = Bodies.circle(450, 50, 20, 20);
+        var buckyC = Bodies.circle(450, 150, 20, 20);
+        var buckyD = Bodies.circle(70, 70, 20, 20);
+
 
         //create ground
         var ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
@@ -75,8 +89,59 @@ document.addEventListener('DOMContentLoaded', function() {
         buckyD.render.sprite.xScale = 2;
         buckyD.render.sprite.yScale = 2;
 
+
+        var block1 = Bodies.rectangle(610, 250, 200, 20, {
+            isStatic: true,
+            angle: Math.PI * 0.06,
+            render: {
+                fillStyle: '#edc51e',
+                strokeStyle: '#b5a91c'
+            }
+        });
+
+        var block2 = Bodies.rectangle(120, 250, 200, 20, {
+            isStatic: true,
+            angle: Math.PI * -0.06,
+            render: {
+                fillStyle: '#edc51e',
+                strokeStyle: '#b5a91c'
+            }
+        });
+
+        var block3 = Bodies.rectangle(380, 150, 400, 20, {
+            isStatic: true,
+            render: {
+                fillStyle: '#edc51e',
+                strokeStyle: '#b5a91c'
+            }
+        });
+
+        var block4 = Bodies.rectangle(380, 350, 400, 20, {
+            isStatic: true,
+            render: {
+                fillStyle: '#edc51e',
+                strokeStyle: '#b5a91c'
+            }
+        });
+
+        var basketleft = Bodies.rectangle(450, 460, 20, 100, {
+            isStatic: true
+        });
+
+        var basketright = Bodies.rectangle(300, 460, 20, 100, {
+            isStatic: true
+        });
+
+        var baskettop = Bodies.rectangle(375, 410, 150, 20, {
+            isStatic: true
+        });
+
+
+
         // add all of the bodies to the world
-        World.add(engine.world, [buckyA, buckyB, buckyC, buckyD, ground]);
+        World.add(engine.world, [buckyA, buckyB, buckyC, buckyD,
+            ground, block1, block2, block3, block4,
+            basketleft, basketright, baskettop]);
 
         // run the engine
         Engine.run(engine);
